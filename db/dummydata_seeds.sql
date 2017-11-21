f51742ae3357859084702aff1634467ebb350be4
ShaToyaJones <jshatoyaj@yahoo.com>
2017-11-21 15:00:29 -0500

    Dummy data has been successfully entered into database. Some database schemas may need to be modified.

--- db/dummydata_seeds.sql
+++ db/dummydata_seeds.sql
@@ -1,36 +1,31 @@
 USE shareTop;
 
-INSERT INTO Equipment (model, speed, ram, screen, price, description);
+INSERT INTO Equipment (model, speed, ram, screen, price, description)
+VALUES ("Dell Inspiron", 2.2, 8, "15.6", 10.00, "This Dell Inspiron notebook computer runs Windows 10 to keep you up to date.");
 
-VALUES ("Dell Inspiron", "2.2GHz", "8GB", "15.6", "$10.00", "Power through your workday with this 15.6-inch Dell Inspiron notebook computer. The expansive 2TB hard drive is ideal for holding photography files and videos securely, and the Intel Core i5 processor and 8GB of RAM let you multitask without sacrificing power. This Dell Inspiron notebook computer runs Windows 10 to keep you up to date.");
+INSERT INTO Equipment (model, speed, ram, screen, price, description)
+VALUES ("Lenovo Intel Pentium", 2.4, 4, "14", 15.00, "This Lenovo IdeaPad 2-in-1 laptop has Windows 10 for an intuitive user interface. The wide-angle touch-screen display of this Lenovo IdeaPad laptop has ultra-thin bezels to let you see more of your videos.");
 
-INSERT INTO Equipment (model, speed, ram, screen, price, description);
+INSERT INTO Equipment (model, speed, ram, screen, price, description)
+VALUES ("Macbook Pro", 2.8, 8, "15", 25.00, "This is a Macbook Pro. What more is there to know?!?!");
 
-VALUES ("Lenovo Intel Pentium", "2.4GHz", "4GB", "14", "$15.00", "Watch videos on the 14-inch HD touch screen of this Lenovo IdeaPad 2-in-1 laptop. Its Intel Pentium processor and 4GB of RAM let you run multiple software programs at once, and the 500GB hard drive offers secure file storage. This Lenovo IdeaPad 2-in-1 laptop has Windows 10 for an intuitive user interface. The wide-angle touch-screen display of this Lenovo IdeaPad laptop has ultra-thin bezels to let you see more of your videos.");
+INSERT INTO Equipment (model, speed, ram, screen, price, description)
+VALUES ("Macbook Air", 1.8, 8, "13", 20.00, "This is a Macbook Air!");
 
-INSERT INTO Equipment (model, speed, ram, screen, price, description);
+INSERT INTO Equipment (model, speed, ram, screen, price, description)
+VALUES ("Microsoft Surface Pro 4", 2.1, 128, "12.3", 24.00, "A very nice Microsoft Surface Pro 4!");
 
-VALUES ("Macbook Pro", "2.8GHz", "8GB", "15", "$25.00", "The new MacBook Pro is razor-thin, featherlight, and now even faster and more powerful than before. It has the brightest, most colorful Mac notebook display. And it features up to 10 hours of battery life.* It’s a notebook built for the work you do every day. Ready to go anywhere a great idea takes you.");
-
-INSERT INTO Equipment (model, speed, ram, screen, price, description);
-
-VALUES ("Macbook Air", "1.8GHz", "8GB", "13", "$20.00", "The 13-inch MacBook Air features 8GB of memory, a fifth-generation Intel Core processor, Thunderbolt 2, great built-in apps, and all-day battery life.* It’s thin, light, and durable enough to take everywhere you go—and powerful enough to do everything once you get there.");
-
-INSERT INTO Equipment (model, speed, ram, screen, price, description);
-
-VALUES ("Microsoft Surface Pro 4", "2.1GHz", "128GB", "12.3", "$24.00", "Use the 12.3 HD touch screen and the Surface Pen to navigate Web pages, write and upload content and play games. The kickstand makes it easy to adjust Surface Pro 4 for hands-free viewing or typing, and Wi-Fi lets you connect to the Internet quickly.");
-
-INSERT INTO UserAccount (emailaddress, password);
+INSERT INTO UserAccount (emailaddress, password)
 VALUES ("shareTop@gmail.com", "1234567");
 
-INSERT INTO UserAccount (emailaddress, password);
+INSERT INTO UserAccount (emailaddress, password)
 VALUES ("shareTop@yahoo.com", "abcdefghij");
 
-INSERT INTO UserAccount (emailaddress, password);
+INSERT INTO UserAccount (emailaddress, password)
 VALUES ("shareTop@hotmail.com", "98756poiji");
 
-INSERT INTO UserAccount (emailaddress, password);
+INSERT INTO UserAccount (emailaddress, password)
 VALUES ("shareTop@outlook.com", "zyxq753");
 
-INSERT INTO UserAccount (emailaddress, password);
+INSERT INTO UserAccount (emailaddress, password)
 VALUES ("jshatoyaj@yahoo.com", "25846q1zyx");
