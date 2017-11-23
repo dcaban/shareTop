b970c29a91af0868492cf4cb39ac7ed281f67040
dcaban <davidjcaban@gmail.com>
2017-11-23 03:33:21 -0500

    styling updated on index and nav bug when selecting time

--- public/assets/js/public.js
+++ public/assets/js/public.js
@@ -6,16 +6,16 @@ $('select').material_select();
 var isLoggedIn = localStorage.setItem("isLoggedOn", false);
 
 $('.timepicker').pickatime({
-    default: 'now', // Set default time: 'now', '1:30AM', '16:30'
-    fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
-    twelvehour: true, // Use AM/PM or 24-hour format
-    donetext: 'OK', // text for done-button
-    cleartext: 'Clear', // text for clear-button
-    canceltext: 'Cancel', // Text for cancel-button
-    autoclose: false, // automatic close timepicker
-    ampmclickable: true, // make AM PM clickable
-    aftershow: function(){} //Function for after opening timepicker
-  });
+  default: 'now', // Set default time: 'now', '1:30AM', '16:30'
+  fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
+  twelvehour: false, // Use AM/PM or 24-hour format
+  donetext: 'OK', // text for done-button
+  cleartext: 'Clear', // text for clear-button
+  canceltext: 'Cancel', // Text for cancel-button
+  autoclose: false, // automatic close timepicker
+  ampmclickable: true, // make AM PM clickable
+  aftershow: function(){} //Function for after opening timepicker
+});
 
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
@@ -26,9 +26,14 @@ $('.timepicker').pickatime({
     closeOnSelect: false // Close upon selecting a date,
   });
 
-    $('.slider').slider({
-      indicators: false
-  });
+    $('.carousel.carousel-slider').carousel({full_width: true, dist: 0});
+
+
+
+
+setInterval(function(){
+$('.carousel').carousel('next');
+}, 8000);
       
 });
        
