f8bae083d08af2b860eecbc39bfe6cfd9412784e
ShaToyaJones <jshatoyaj@yahoo.com>
2017-11-28 18:35:43 -0500

    STILL working on ensuring the api routes are properly setupgit add .!

--- routes/apiRoutes/api-routes.js
+++ routes/apiRoutes/api-routes.js
@@ -12,73 +12,223 @@ var db = require("../models");
 // =============================================================
 module.exports = function(app) {
 
-  // GET route for getting all of the posts
-  app.get("/api/posts/", function(req, res) {
-    db.Post.findAll({})
-    .then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
+  // Add equipment
+  app.post("/api/new", function(req, res) {
+    
+        console.log("Equipment Data:");
+        console.log(req.body);
+    
+        Equipment.create({
+          model: req.body.model,
+          speed: req.body.speed,
+          ram: req.body.ram,
+          screen_size: req.body.screen_size,
+          price: req.body.price,
+          description: req.body.description     
+        }).then(function(results) {
+          // `results` here would be the newly created piece of equipment
+          res.end();
+        });
+      });
 
-  // Get route for returning posts of a specific model
-  app.get("/api/posts/model/:model", function(req, res) {
-    db.Post.findAll({
-      where: {
-        model: req.params.model
-      }
-    })
-    .then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
+  // Add user account
+  app.post("/api/new", function(req, res) {
+    
+        console.log("useraccount Data:");
+        console.log(req.body);
+    
+        userAccount.create({
+          emailaddress: req.body.emailaddress,
+          password: req.body.password        
+        }).then(function(results) {
+          // `results` here would be the newly created user.
+          res.end();
+        });
+      });
 
-  // Get rotue for retrieving a single post
-  app.get("/api/posts/:id", function(req, res) {
-    db.Post.findOne({
+  // Add customer
+  app.post("/api/new", function(req, res) {
+    
+        console.log("Customer Data:");
+        console.log(req.body);
+    
+        Customer.create({
+          first_name: req.body.first_name,
+          last_name: req.body.last_name,
+          address: req.body.address,
+          address2: req.body.address2,
+          city: req.body.city,
+          state: req.body.state,
+          postal_code: req.body.postal_code,
+          phone: req.body.phone,
+          create_date: req.body.create_date,
+          last_update: req.body.last_update     
+        }).then(function(results) {
+          // `results` here would be the newly created customer.
+          res.end();
+        });
+      });
+
+  // DELETE route for deleting equipment
+  app.delete("/api/equipment/:id", function(req, res) {
+    db.Equipment.destroy({
       where: {
         id: req.params.id
       }
     })
-    .then(function(dbPost) {
-      res.json(dbPost);
+    .then(function(dbEquipment) {
+      res.json(dbEquipment);
     });
   });
 
-  // POST route for saving a new post
-  app.post("/api/posts", function(req, res) {
-    console.log(req.body);
-    db.Post.create({
-      title: req.body.title,
-      body: req.body.body,
-      category: req.body.category
-    })
-    .then(function(dbPost) {
-      res.json(dbPost);
-    });
-  });
-
-  // DELETE route for deleting posts
-  app.delete("/api/posts/:id", function(req, res) {
-    db.Post.destroy({
+  // DELETE route for deleting user account
+  app.delete("/api/useraccount/:id", function(req, res) {
+    db.UserAccount.destroy({
       where: {
         id: req.params.id
       }
     })
-    .then(function(dbPost) {
-      res.json(dbPost);
+    .then(function(dbUserAccount) {
+      res.json(dbUserAccount);
     });
   });
 
-  // PUT route for updating posts
-  app.put("/api/posts", function(req, res) {
-    db.Post.update(req.body,
+  // PUT route for updating equipment
+  app.put("/api/equpiment", function(req, res) {
+    db.Equipment.update(req.body,
       {
         where: {
           id: req.body.id
         }
       })
-    .then(function(dbPost) {
-      res.json(dbPost);
+    .then(function(dbEquipment) {
+      res.json(dbEquipment);
     });
   });
+
+  // PUT route for updating password in useraccount
+  app.put("/api/useraccount", function(req, res) {
+    db.UserAccount.update(req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      })
+    .then(function(dbUserAccount) {
+      res.json(dbUserAccount);
+    });
+  });
+
+  // PUT route for updating user account
+  app.put("/api/useraccount", function(req, res) {
+    db.UserAccount.update(req.body,
+      {
+        where: {
+          id: req.body.id
+        }
+      })
+    .then(function(dbUserAccount) {
+      res.json(dbUserAccount);
+    });
+  });
+
+    // Search for specific equipment (or all equipment) then provides JSON
+    app.get("/api/:equipment?", function(req, res) {
+      
+          // If the user provides a specific piece of equipment in the URL...
+          if (req.params.equipment) {
+      
+            // Then display the JSON for ONLY that equipment.
+            // (Note how we're using the ORM here to run our searches)
+            Equipment.findOne({
+              where: {
+                routeName: req.params.equipment
+              }
+            }).then(function(result) {
+              return res.json(result);
+            });
+          }
+      
+          // Otherwise...
+          else {
+            // Otherwise display the data for all of the equipment.
+            // (Note how we're using Sequelize here to run our searches)
+            Equipment.findAll({})
+              .then(function(result) {
+                return res.json(result);
+              });
+          }
+      
+        });
+    //*** Unsure if this is needed?????***
+    // GET route for getting all of the posts
+    app.get("/api/posts/", function(req, res) {
+      db.Post.findAll({})
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+    });
+  
+    // Get route for returning posts of a specific model
+    app.get("/api/posts/model/:model", function(req, res) {
+      db.Post.findAll({
+        where: {
+          model: req.params.model
+        }
+      })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+    });
+  
+    // Get rotue for retrieving a single post
+    app.get("/api/posts/:id", function(req, res) {
+      db.Post.findOne({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+    });
+  
+    // POST route for saving a new post
+    app.post("/api/posts", function(req, res) {
+      console.log(req.body);
+      db.Post.create({
+        title: req.body.title,
+        body: req.body.body,
+        category: req.body.category
+      })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+    });
+  
+    // DELETE route for deleting posts
+    app.delete("/api/posts/:id", function(req, res) {
+      db.Post.destroy({
+        where: {
+          id: req.params.id
+        }
+      })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+    });
+  
+    // PUT route for updating posts
+    app.put("/api/posts", function(req, res) {
+      db.Post.update(req.body,
+        {
+          where: {
+            id: req.body.id
+          }
+        })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+    });  
+
 };
