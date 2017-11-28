0dc7a1fb0af5850478c75d8282094a9435b3e466
dcaban <davidjcaban@gmail.com>
2017-11-28 18:34:27 -0500

    lots of handlebar pages and partials added/ sliders now working

--- routes/htmlRoutes/appRoutes.js
+++ routes/htmlRoutes/appRoutes.js
@@ -4,11 +4,21 @@ var router = express.Router();
 
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/form", function(req, res) {
+router.get("/signup", function(req, res) {
     res.render("createUser");
   });
   router.get("/", function(req, res) {
     res.render("index");
+  });
+  router.get("/login", function(req, res) {
+    res.render("login");
+  });
+
+  router.get("/results", function(req, res) {
+    res.render("results");
+  });
+  router.get("/admin", function(req, res) {
+    res.render("admin");
   });
 
 // Export routes for server.js to use.
