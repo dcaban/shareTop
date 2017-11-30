80b41e38a0fa6ae0bad4d7666d045edd29a4c652
Bayrex Rodriguez <bayrexr@gmail.com>
2017-11-30 01:33:33 -0500

    Adds work to create user post route.

--- controllers/users.js
+++ controllers/users.js
@@ -2,10 +2,15 @@
 //====//
 //=====//
 const User = require('../models/customer');
+const express = require('express');
+
 
 module.exports = {
     signUp: async (req, res, next) => {
+        console.log('req.body');
+        console.log(req.body);
         console.log('Called: userController.signUp()');
+        
     },
 
     signIn: async (req, res, next) => {
