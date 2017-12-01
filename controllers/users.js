2ab24ce02d0f16b04bc00ffae2950bb90414c6d6
Bayrex Rodriguez <bayrexr@gmail.com>
2017-11-30 22:02:52 -0500

    Adds sequilize to users.js

--- controllers/users.js
+++ controllers/users.js
@@ -2,20 +2,21 @@
 //=====//
 //====//
 //=====//
-const User = require('../models/customer');
+// const User = require('../models/customer');
 const express = require('express');
 const Joi = require('joi');
+const db = require('../models');
 
 //Validation Schema
 const userSchema = Joi.object().keys({
-    email: Joi.string().email().required(),
+    emailaddress: Joi.string().email().required(),
     password: Joi.string().regex(/^[a-zA-Z0-9!@$~?-_ ]{3,20}$/).required(),
     confirmPassword: Joi.any().valid(Joi.ref('password')).required(),
-    firstName: Joi.string().regex(/^[a-zA-Z ]{3,50}$/),
-    lastName: Joi.string().regex(/^[a-zA-Z -]{3,50}$/),
+    first_name: Joi.string().regex(/^[a-zA-Z ]{3,50}$/),
+    last_name: Joi.string().regex(/^[a-zA-Z -]{3,50}$/),
     address: Joi.string().regex(/^[a-zA-Z0-9_]+( [a-zA-Z0-9_]+)*$/),
     city: Joi.string().regex(/^[a-zA-Z ]{3,50}$/),
-    zipCode: Joi.string().regex(/^[0-9]{5}$/),
+    postal_code: Joi.string().regex(/^[0-9]{5}$/),
     state: Joi.any(),
     action: Joi.any()
 });
@@ -38,6 +39,9 @@ module.exports = {
         }
         
         //post sequalize functions
+        db.Customer.create(req.body).then(function(dbCustomer) {
+            res.json(dbCustomer);
+        });
     },
 
     signIn: async (req, res, next) => {
