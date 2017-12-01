0112686b1f3770b4e7a5e0caf80a507e01d710d8
dcaban <davidjcaban@gmail.com>
2017-12-01 16:02:37 -0500

    merging heroku

--- public/assets/js/find.js
+++ public/assets/js/find.js
@@ -10,8 +10,8 @@ function latLong() {
          var foundSpeed = "CPU: " + found[i].speed + "G";
          var foundRam = "   | Ram: " + found[i].ram + "G";
          var foundScreen = " | Screen Size: " + found[i].screen_size +" inch"+ h;
-         var foundDescription = "Description " + found[i].description;
-         var cardTitle = "<div class='col s12 l6'><div class='card search card'><div class='card-image'><img src='/assets/img/handbook.jpeg'><span class='card-title'>" + name + "</span> </div><div class='card-content'><p>"
+         var foundDescription = found[i].description;
+         var cardTitle = "<div class='col s12 xl6'><div class='card searchcard'><div class='card-image'><img src='/assets/img/handbook.jpeg'><span class='card-title'>" + name + "</span> </div><div class='card-content'><p>"
          var cardContent = foundModels + foundSpeed + foundRam + foundScreen + foundDescription;
          var cardEnd= "</p></div><div class='card-action'><a href='#'>This is a link</a></div></div></div></div>"
 
